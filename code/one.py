5a02e648329ea4927c147413f550925780ee133e
Priyanka29598 <51356648+Priyanka29598@users.noreply.github.com>
2019-06-04 12:21:43 +0530

    Update one.py

--- code/one.py
+++ code/one.py
@@ -1,1 +1,9 @@
-print("hello")
+v=input("Enter a value")
+if(v>0):
+  print("entered value is positive")
+elif(v<0):
+  print("entered value is negative")
+elif(v==0):
+  print("The value is zero")
+else:
+  print("Enter a valid number")
