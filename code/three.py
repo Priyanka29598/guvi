9d7be45f7a454b3074280600d47aececcfef6181
Priyanka29598 <51356648+Priyanka29598@users.noreply.github.com>
2019-06-04 14:31:25 +0530

    Update three.py

--- code/three.py
+++ code/three.py
@@ -1,4 +1,4 @@
-s=input("Enter a character")
+s=input()
 if(s=='a' or s=='e' or s=='i' or s=='o' or s=='u'):
    print("Vowel")
 elif(s=='b' or s=='c' or s=='d' or s=='f' or s=='g'or s=='h' or s=='j'or s=='k' or s=='l' or s=='m' or s=='n' or s=='p' or s=='q' or s=='r' or s=='s'or s=='t' or s=='v'or s=='w' or s=='x' or s=='y' or s=='z'):
