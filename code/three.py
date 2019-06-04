360c6ba0048a90edc55c6fd167bd2bf0279c4094
Priyanka29598 <51356648+Priyanka29598@users.noreply.github.com>
2019-06-04 14:24:51 +0530

    Update three.py

--- code/three.py
+++ code/three.py
@@ -1,8 +1,7 @@
 s=input("Enter a character")
-if(s=='a' or s=='e' or s=='i' or s=='o' or s=='u'or s=='A' or s=='E'or s=='I' or s=='O' or s=='U')
+if(s=='a' or s=='e' or s=='i' or s=='o' or s=='u'):
    print("Vowel")
-elif(s=='b' or s=='c' or s=='d' or s=='f' or s=='g'or s=='h' or s=='j'or s=='k' or s=='l' or s=='m' or s=='n' or s=='p' or s=='q' or s=='r' or s=='s'or s=='t' or s=='v'or s=='w' or s=='x' or s=='y' or s=='z' or s=='B' or s=='C' or s=='D' or s=='F'or s=='G' or s=='H'or s=='J' or s=='K' or s=='L'
-s=='M' or s=='N' or s=='P' or s=='Q' or s=='R'or s=='S' or s=='T'or s=='V' or s=='W' or s=='X' or s=='Y' or s=='Z'):
-   print("Consonant"
+elif(s=='b' or s=='c' or s=='d' or s=='f' or s=='g'or s=='h' or s=='j'or s=='k' or s=='l' or s=='m' or s=='n' or s=='p' or s=='q' or s=='r' or s=='s'or s=='t' or s=='v'or s=='w' or s=='x' or s=='y' or s=='z'):
+   print("Consonant")
 else:
    print("invalid")
