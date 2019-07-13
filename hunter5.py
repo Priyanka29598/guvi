58e5a42acb442bdeb300b51ba5a39f76581117fd
Priyanka29598 <51356648+Priyanka29598@users.noreply.github.com>
2019-07-13 18:45:14 +0530

    Create hunter5.py

---
+++ hunter5.py
@@ -0,0 +1,12 @@
+pk=list(map(str,input()))
+v=t=0
+for i in range(0,len(pk)-1):
+  q=pk[i]
+  if int(q)!=0:
+   for j in range(i+1,i+2):
+    q=q+pk[j]
+    if int(q)<27 and int(q)>0: v=v+1
+    elif int(q)==0: v=v-1
+    else: break
+if v!=1: t=v%2
+print(v+t+1)
