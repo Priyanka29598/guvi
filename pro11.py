5138156efde8e00f536df1ef81518c6717ae2546
Priyanka29598 <51356648+Priyanka29598@users.noreply.github.com>
2019-07-13 21:16:38 +0530

    Update pro11.py

--- pro11.py
+++ pro11.py
@@ -1,9 +1,5 @@
-ip=int(input())
-cp=0
-l=[]
-for i in range(1,i+1):
-  l.append(i)
-for i in range(len(l)):
-  for i in range(i+1,len(l)):
-    cp+=1
-print(cp)
+at=int(input())
+if at%2==0:
+    print(at*(at//2)-(at//2))
+else:
+    print(at*(at//2))
